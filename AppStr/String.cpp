e6c7d029a65a51a70d6c364b8356493e041df9bd
Arbalest163 <evil_xacker@mail.ru>
2021-08-29 13:56:17 +0400

    Мелкие испрвления

--- AppStr/String.cpp
+++ AppStr/String.cpp
@@ -1,14 +1,11 @@
 #include "String.h"
 
-int String::count{ 0 };
 
 String::~String()
 {
 	if (_string != nullptr)
 	{
-		std::cout << "Destroy " << name <<  "\n";
 		delete[] _string;
-		count--;
 	}
 }
 
@@ -27,17 +24,11 @@ String::String(const char* string): _string(nullptr), _size(0)
 		strcpy_s(_string, _size + 1, string);
 		_string[_size] = '\0';
 	}
-	count++;
-	name.append(std::to_string(count));
-	std::cout << "Constructor default for " << name << "\n";
 }
 
 String::String(const String& Obj)
 {
 	*this = Obj;
-	count++;
-	name.append(std::to_string(count));
-	std::cout << "Constructor Copy for " << name << "\n";
 }
 
 String::String(const String& Obj_1, const String& Obj_2)
@@ -47,14 +38,10 @@ String::String(const String& Obj_1, const String& Obj_2)
 	strcat_s(_string, _size + 1, Obj_1._string);
 	strcat_s(_string, _size + 1, Obj_2._string);
 	_string[_size] = '\0';
-	count++;
-	name.append(std::to_string(count));
-	std::cout << "Constructor two parameters for " << name << "\n";
 }
 
 String& String::operator=(const String& Obj)
 {
-	std::cout << "Operator =  for " << name << "\n";
 	if (this == &Obj)
 	{
 		return *this;
